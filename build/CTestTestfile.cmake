c1221978dbee27e5ef57c72d29abffb5b3754fad
dzx-30 <2657636715@qq.com>
2025-02-25 12:04:50 +0800

    oneBox

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nf/basket/src
-# Build directory: /home/nf/basket/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basket")
--- build/basket/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nf/basket/src/basket
-# Build directory: /home/nf/basket/build/basket
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
