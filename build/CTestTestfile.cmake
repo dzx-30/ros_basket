ddbff446269e760535905373518a2b932dd43877
dzx-30 <2657636715@qq.com>
2025-02-25 12:01:12 +0800

    oneBox

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dzx/Documents/basket/src
-# Build directory: /home/dzx/Documents/basket/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basket")
